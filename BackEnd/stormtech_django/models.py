5e815c2a596c8427cf8e2f51fed6a06f63f85ae5
danielyoshizawa <yoshidanielcwb@gmail.com>
2016-11-14 15:45:39 -0200

    Adding Sort Books Route to the backend

--- BackEnd/stormtech_django/models.py
+++ BackEnd/stormtech_django/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=256, blank=False, null=False, default='')
     author = models.CharField(max_length=256, blank=False, null=False, default='')
-    year = models.IntegerField(max_length=4, blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.title
--- BackEnd/stormtech_django/views.py
+++ BackEnd/stormtech_django/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import list_route, detail_route
+from rest_framework.response import Response
+
 from stormtech_django.models import Book
 from stormtech_django.serializer import BookSerializer
 
@@ -7,3 +10,16 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    @list_route()
+    def sorted_books(self, request):
+        books = self.get_object()
+
+        return Response(200)
+
+    @detail_route(methods=['post'])
+    def sort_books(self, request):
+        instance = self.get_object()
+        sorted(instance, key= lambda book: book.title)
+        serializer = BookSerializer(instance)
+
+        return Response(serializer.data);
