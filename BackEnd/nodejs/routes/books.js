d4898215adbb416c100b22f51d6efa520179fc19
danielyoshizawa <yoshidanielcwb@gmail.com>
2016-11-15 14:36:18 -0200

    Accepting Multiple Field to be sorted

--- BackEnd/nodejs/routes/books.js
+++ BackEnd/nodejs/routes/books.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-// TODO : Move it to its own class
-
 var sortBooks = function (books, field, sort) {
-
     var control = 1;
 
     if (sort == "desc") {
@@ -20,14 +17,50 @@ var sortBooks = function (books, field, sort) {
     return books;
 };
 
+var isBooksListValid = function(books) {
+
+    return books == null ? false : true;
+};
+
+var validateBooksList = function(res, books) {
+    if (books == null) {
+        res.status(500).send("Sorting Service Exception");
+        return false;
+    }  else {
+        return true;
+    }
+};
+
+router.post('/sort', function(req, res, next) {
+
+    if(!validateBooksList(res,req.body.books)) {
+        return;
+    }
+
+    var books = req.body.books;
+    var queryFields = Object.keys(req.query);
+
+    for (var i = queryFields.length - 1; i >= 0; i--) {
+        books = sortBooks(books, queryFields[i], req.query[i] == "desc" ? "desc" : "asc");
+    }
+
+    res.send(books);
+});
+
 router.post('/sortByTitle', function(req, res, next) {
 
-    console.log(req.query.sort);
+    if(!validateBooksList(res,req.body.books)) {
+        return;
+    }
 
     res.send(sortBooks(req.body.books, "title", req.query.sort == "desc" ? "desc" : "asc"));
 });
 
 router.post('/sortByAuthor', function(req, res, next) {
+
+    if(!validateBooksList(res,req.body.books)) {
+        return;
+    }
 
     res.send(sortBooks(req.body.books, "author", req.params.sort == "desc" ? "desc" : "asc"));
 });
